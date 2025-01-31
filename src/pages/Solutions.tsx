97ea5d34b532c9ef0640147eb7910d08d3450a63
apoorvaa <apoorvaagowda08@gmail.com>
2025-01-31 12:17:50 +0530

    fixed

--- src/pages/Solutions.tsx
+++ src/pages/Solutions.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { FaSearch, FaChartLine, FaIndustry, FaClipboardCheck, FaTasks, FaUsers, FaEye, FaShieldAlt, FaTools } from 'react-icons/fa';
-import solutionsBanner from '../assets/solutionsBanner.png'; // Ensure correct path and capitalization
+import solutionsBanner from "../assets/solutionsBanner.png";
+
+
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
